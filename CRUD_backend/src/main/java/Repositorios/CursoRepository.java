0563f3a2a7f0d1303f04181b688fec7f7ca28b42
Ilko Esteban Arboit <ilkoarboit@gmail.com>
2023-02-20 19:51:21 -0300

    Actualización de repositorios +Actualización de repositorios de Curso y Profesor

--- CRUD_backend/src/main/java/Repositorios/CursoRepository.java
+++ CRUD_backend/src/main/java/Repositorios/CursoRepository.java
@@ -1,38 +1,57 @@
 package Repositorios;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import Entidades.Alumno;
 import Entidades.Curso;
 import Entidades.Profesor;
 
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, String> {
 
-    @Query("SELECT c FROM Curso c JOIN c.profesor p WHERE p = :profesor AND c.turno = :turno")
-    List<Curso> findByProfesorAndTurno(@Param("profesor") Profesor profesor, @Param("turno") String turno);
+    //Buscar un curso por ID
+    @Query("SELECT c FROM Curso c WHERE c.id = :id")
+    Optional<Curso> findById(@Param("id") String id);
 
+    // Buscar un cursopor Nombre
+    @Query("SELECT c FROM Curso c WHERE c.nombre = :nombre")
+    Curso findByNombre(@Param("nombre") String nombre);
+
+    //Buscar un curso por Nombre y Turno
+    @Query("SELECT c FROM Curso c WHERE c.nombre = :nombre AND c.turno = :turno")
+    Curso findByNombreAndTurno(@Param("nombre") String nombre, @Param("turno") String turno);
+
+    //Buscar un curso por dias
     @Query("SELECT c FROM Curso c WHERE c.dias LIKE %:dias%")
     List<Curso> findByDias(@Param("dias") String dias);
 
+    //Buscar un curso por profesor
     @Query("SELECT c FROM Curso c WHERE c.profesor = :profesor")
     List<Curso> findByProfesor(@Param("profesor") Profesor profesor);
 
+    //Buscar un curso por turno
     @Query("SELECT c FROM Curso c WHERE c.turno = :turno")
     List<Curso> findByTurno(@Param("turno") String turno);
 
+    //Buscar un curso por Dia y turno
     @Query("SELECT c FROM Curso c WHERE c.dias LIKE %:dia% AND c.turno = :turno")
     List<Curso> findByDiaAndTurno(@Param("dia") String dia, @Param("turno") String turno);
 
+    //Buscar un curso por Dia y profesor
     @Query("SELECT c FROM Curso c WHERE c.dias LIKE CONCAT('%',:dia,'%') AND c.profesor.nombre LIKE CONCAT('%',:nombreProfesor,'%')")
     List<Curso> findByDiaAndNombreProfesor(@Param("dia") String dia, @Param("nombreProfesor") String nombreProfesor);
 
+    //Buscar un curso por Nombre del profesor y el turno
     @Query("SELECT c FROM Curso c WHERE c.turno=:turno AND c.profesor.nombre LIKE CONCAT('%',:nombreProfesor,'%')")
     List<Curso> findByTurnoAndNombreProfesor(@Param("turno") String turno, @Param("nombreProfesor") String nombreProfesor);
 
+    // Busca cursos por profesor, turno y días
+    @Query("SELECT c FROM Curso c WHERE c.profesor.id = :idProfesor AND c.turno = :turno AND c.dias LIKE %:dias%")
+    Curso findByProfesorAndTurnoAndDias(@Param("idProfesor") String idProfesor, @Param("turno") String turno, @Param("dias") String dias);
 }
 
--- CRUD_backend/src/main/java/Repositorios/ProfesorRepository.java
+++ CRUD_backend/src/main/java/Repositorios/ProfesorRepository.java
@@ -17,12 +17,18 @@ public interface ProfesorRepository extends JpaRepository<Profesor, String> {
     @Query("SELECT a FROM profesor a WHERE a.id = :id")
     Optional<Profesor> findById(@Param("id") String id);
 
-    @Query("SELECT p FROM Profesor p WHERE p.email = :email")
-    Profesor findByEmail(@Param("email") String email);
-    
+    @Query("SELECT p FROM Profesor p WHERE p.apellido = :apellido")
+    List<Profesor> findByApellido(@Param("apellido") String apellido);
+
     @Query("SELECT p FROM Profesor p WHERE p.apellido = :apellido AND p.nombre = :nombre")
     List<Profesor> findByApellidoAndNombre(@Param("apellido") String apellido, @Param("nombre") String nombre);
-    
+
+    @Query("SELECT p FROM Profesor p WHERE p.email = :email")
+    Profesor findByEmail(@Param("email") String email);
+
     @Query("SELECT p FROM Profesor p JOIN p.cursos c WHERE c = :curso AND p.turno = :turno")
     List<Profesor> findByCursoAndTurno(@Param("curso") Curso curso, @Param("turno") String turno);
+
+    @Query("SELECT p FROM Profesor p JOIN p.cursos c WHERE p.id = :id AND c.turno = :turno AND c.dias LIKE %:dias%")
+Optional<Profesor> findByIdAndTurnoAndDias(@Param("id") String id, @Param("turno") String turno, @Param("dias") String dias);
 }
